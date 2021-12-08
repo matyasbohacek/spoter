d4c6a4cf1f26a240c39576dbc01e87e068be8681
Matyáš Boháček <matyas.bohacek@matsworld.io>
2021-12-08 16:30:38 +0100

    Add comments, parameter help reference, etc.

--- train.py
+++ train.py
@@ -24,35 +24,53 @@ from cslr_transformer.gaussian_noise import GaussianNoise
 def get_default_args():
     parser = argparse.ArgumentParser(add_help=False)
 
-    parser.add_argument("--experiment_name", type=str, default="lsa_64_spot", help="Name of the experiment after which the logs and plots will be named")
+    parser.add_argument("--experiment_name", type=str, default="lsa_64_spoter",
+                        help="Name of the experiment after which the logs and plots will be named")
     parser.add_argument("--num_classes", type=int, default=64, help="Number of classes to be recognized by the model")
-    parser.add_argument("--hidden_dim", type=int, default=108, help="Hidden dimension of the underlying Transformer model")
-    parser.add_argument("--seed", type=int, default=379, help="Seed with which to initialize all the random components of the training")
+    parser.add_argument("--hidden_dim", type=int, default=108,
+                        help="Hidden dimension of the underlying Transformer model")
+    parser.add_argument("--seed", type=int, default=379,
+                        help="Seed with which to initialize all the random components of the training")
 
     # Data
-    parser.add_argument("--training_set", type=str, default="/storage/plzen4-ntis/projects/cv/spot/LSA64_60fps.csv", help="Path to the training dataset CSV file")
-    parser.add_argument("--validation_set", type=str, default="", help="Path to the validation dataset CSV file")
-    parser.add_argument("--testing_set", type=str, default="", help="Path to the testing dataset CSV file")
+    parser.add_argument("--training_set_path", type=str, default="", help="Path to the training dataset CSV file")
+    parser.add_argument("--testing_set_path", type=str, default="", help="Path to the testing dataset CSV file")
+    parser.add_argument("--experimental_train_split", type=float, default=None,
+                        help="Determines how big a portion of the training set should be employed (intended for the "
+                             "gradually enlarging training set experiment from the paper)")
+
+    parser.add_argument("--validation_set", type=str, choices=["from-file", "split-from-train", "none"],
+                        default="from-file", help="Type of validation set construction. See README for further rederence")
+    parser.add_argument("--validation_set_size", type=float,
+                        help="Proportion of the training set to be split as validation set, if 'validation_size' is set"
+                             " to 'split-from-train'")
+    parser.add_argument("--validation_set_path", type=str, default="", help="Path to the validation dataset CSV file")
 
     # Training hyperparameters
     parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train the model for")
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate for the model training")
-    parser.add_argument("--log_freq", type=int, default=1, help="Log frequency (frequency of printing all the training info)")
+    parser.add_argument("--log_freq", type=int, default=1,
+                        help="Log frequency (frequency of printing all the training info)")
 
     # Checkpointing
-    parser.add_argument("--save_checkpoints", type=bool, default=True, help="Determines whether to save weights checkpoints")
+    parser.add_argument("--save_checkpoints", type=bool, default=True,
+                        help="Determines whether to save weights checkpoints")
 
     # Scheduler
     parser.add_argument("--scheduler_factor", type=int, default=0.1, help="Factor for the ReduceLROnPlateau scheduler")
-    parser.add_argument("--scheduler_patience", type=int, default=5, help="Patience for the ReduceLROnPlateau scheduler")
+    parser.add_argument("--scheduler_patience", type=int, default=5,
+                        help="Patience for the ReduceLROnPlateau scheduler")
 
     # Gaussian noise normalization
     parser.add_argument("--gaussian_mean", type=int, default=0, help="Mean parameter for Gaussian noise layer")
-    parser.add_argument("--gaussian_std", type=int, default=0.001, help="Standard deviation parameter for Gaussian noise layer")
+    parser.add_argument("--gaussian_std", type=int, default=0.001,
+                        help="Standard deviation parameter for Gaussian noise layer")
 
-    parser.add_argument("--plot_stats", type=bool, default=True, help="Determines whether continuous statistics should be plotted at the end")
-    parser.add_argument("--plot_lr", type=bool, default=True, help="Determines whether the LR should be plotted at the end")
-    parser.add_argument("--experimental_train_split", type=float, default=0.8)
+    # Visualization
+    parser.add_argument("--plot_stats", type=bool, default=True,
+                        help="Determines whether continuous statistics should be plotted at the end")
+    parser.add_argument("--plot_lr", type=bool, default=True,
+                        help="Determines whether the LR should be plotted at the end")
 
     return parser
 
@@ -88,7 +106,6 @@ def train(args):
 
     # Construct the model
     slrt_model = CSLRTransformer(num_classes=args.num_classes, hidden_dim=args.hidden_dim)
-    # vision_tt = VisionTransformer(dim=2, mlp_dim=108, num_classes=100, depth=12, heads=8)
     slrt_model.train(True)
     slrt_model.to(device)
 
@@ -106,38 +123,36 @@ def train(args):
 
     # Training set
     transform = transforms.Compose([GaussianNoise(args.gaussian_mean, args.gaussian_std)])
-    train_set = CzechSLRDataset(args.training_set, transform=transform, augmentations=True)
+    train_set = CzechSLRDataset(args.training_set_path, transform=transform, augmentations=True)
 
+    # Validation set
+    if args.validation_set == "from-file":
+        val_set = CzechSLRDataset(args.validation_set_path)
+        val_loader = DataLoader(val_set, shuffle=True, generator=g)
 
-    if True:
+    elif args.validation_set == "split-from-train":
         train_set, val_set = __balance_val_split(train_set, 0.2)
 
         val_set.transform = None
         val_set.augmentations = False
         val_loader = DataLoader(val_set, shuffle=True, generator=g)
 
-        train_set = __split_of_train_sequence(train_set, args.experimental_train_split)
-
-
-    train_loader = DataLoader(train_set, shuffle=True, generator=g)
-
-    # Validation set
-    if args.validation_set:
-        #val_set = CzechSLRDataset(args.validation_set)
-        #val_loader = DataLoader(val_set, shuffle=True, generator=g)
-        pass
-
     else:
-        #val_loader = None
-        pass
+        val_loader = None
 
     # Testing set
-    if args.testing_set:
-        eval_set = CzechSLRDataset(args.testing_set)
+    if args.testing_set_path:
+        eval_set = CzechSLRDataset(args.testing_set_path)
         eval_loader = DataLoader(eval_set, shuffle=True, generator=g)
 
     else:
         eval_loader = None
+
+    # Final training set refinements
+    if args.experimental_train_split:
+        train_set = __split_of_train_sequence(train_set, args.experimental_train_split)
+
+    train_loader = DataLoader(train_set, shuffle=True, generator=g)
 
 
     # MARK: TRAINING
@@ -147,11 +162,15 @@ def train(args):
     top_train_acc, top_val_acc = 0, 0
     checkpoint_index = 0
 
-    print("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
-    logging.info("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
+    if args.experimental_train_split:
+        print("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
+        logging.info("Starting " + args.experiment_name + "_" + str(args.experimental_train_split).replace(".", "") + "...\n\n")
+
+    else:
+        print("Starting " + args.experiment_name + "...\n\n")
+        logging.info("Starting " + args.experiment_name + "...\n\n")
 
     for epoch in range(args.epochs):
-        # train_loss, _, _, train_acc = train_epoch(slrt_model, train_loader, cel_criterion, sgd_optimizer, device, scheduler)
         train_loss, _, _, train_acc = train_epoch(slrt_model, train_loader, cel_criterion, sgd_optimizer, device)
         losses.append(train_loss.item() / len(train_loader))
         train_accs.append(train_acc)
@@ -188,14 +207,6 @@ def train(args):
             top_train_acc, top_val_acc = 0, 0
             checkpoint_index += 1
 
-            # TEMP
-            # slrt_model.train(False)
-            # _, _, eval_acc = evaluate(slrt_model, eval_loader, device)
-            # slrt_model.train(True)
-            # print("\n! TEST ACC: " + str(eval_acc) + "\n")
-            # logging.info("\n! TEST ACC: " + str(eval_acc) + "\n")
-            # TEMP
-
         lr_progress.append(sgd_optimizer.param_groups[0]["lr"])
 
     # MARK: TESTING
@@ -225,7 +236,7 @@ def train(args):
         logging.info("\nThe top result was recorded at " + str(top_result) + " testing accuracy. The best checkpoint is " + top_result_name + ".")
 
 
-    # PLOTTING 0: Performance (loss, accuracies) chart plotting
+    # PLOT 0: Performance (loss, accuracies) chart plotting
     if args.plot_stats:
         fig, ax = plt.subplots()
         ax.plot(range(1, len(losses) + 1), losses, c="#D64436", label="Training loss")
@@ -241,9 +252,8 @@ def train(args):
         ax.grid()
 
         fig.savefig("out-img/" + args.experiment_name + "_loss.png")
-        # plt.show()
 
-    # PLOTTING 1: Learning rate progress
+    # PLOT 1: Learning rate progress
     if args.plot_lr:
         fig1, ax1 = plt.subplots()
         ax1.plot(range(1, len(lr_progress) + 1), lr_progress, label="LR")
@@ -251,7 +261,6 @@ def train(args):
         ax1.grid()
 
         fig1.savefig("out-img/" + args.experiment_name + "_lr.png")
-        # plt.show()
 
     print("\nAny desired statistics have been plotted.\nThe experiment is finished.")
     logging.info("\nAny desired statistics have been plotted.\nThe experiment is finished.")
